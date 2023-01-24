6b9583ea56d6f31ad92d6184cfdbe76a3e59e73e
Asif <asifsaheer7034@gmail.com>
2023-01-24 17:46:04 +0530

    updated

---
+++ src/Pages/admin/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from "../../components/admin/sidebar/Sidebar"
+
+function Home() {
+  return (
+    <div>
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/Pages/admin/Project.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Projects from "../../components/admin/project/Project";
+import Sidebar from "../../components/admin/sidebar/Sidebar";
+
+function Project() {
+  return (
+    <div>
+      <Sidebar />
+      <Projects />
+    </div>
+  );
+}
+
+export default Project;
